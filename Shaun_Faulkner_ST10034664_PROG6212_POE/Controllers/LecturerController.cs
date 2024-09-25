8c254ec29f10ab85715198f437f732649ef3b327
Sfaulcon <shaun.faulkner.19@gmail.com>
2024-09-25 20:21:41 +0200

    functionality changes

--- Shaun_Faulkner_ST10034664_PROG6212_POE/Controllers/LecturerController.cs
+++ Shaun_Faulkner_ST10034664_PROG6212_POE/Controllers/LecturerController.cs
@@ -1,15 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shaun_Faulkner_ST10034664_PROG6212_POE.Services;
 
 namespace Shaun_Faulkner_ST10034664_PROG6212_POE.Controllers
 {
     public class LecturerController : Controller
     {
-        public ActionResult Dashboard()
+        private readonly BlobStorageService _blobStorageService;
+        private readonly TableStorageService _tableStorageService;
+
+        public LecturerController()
         {
-            return View();
+            _blobStorageService = new BlobStorageService("<blob connection string>", "<container name>");
+            _tableStorageService = new TableStorageService("<table connection string>", "<table name>");
         }
 
-        public ActionResult SubmitClaim()
+        [HttpPost]
+        public async Task<IActionResult> SubmitClaim(IFormFile SupportingDocuments, string Name, string Surname, string Email, int HoursWorked)
+        {
+            string documentUrl = null;
+
+            if (SupportingDocuments != null && SupportingDocuments.Length > 0)
+            {
+                using (var stream = SupportingDocuments.OpenReadStream())
+                {
+                    documentUrl = await _blobStorageService.UploadFileAsync(stream, SupportingDocuments.FileName);
+                }
+            }
+
+            await _tableStorageService.SaveClaimAsync(Name, Surname, Email, HoursWorked, documentUrl);
+            return RedirectToAction("Dashboard");
+        }
+
+        public ActionResult Dashboard()
         {
             return View();
         }
---
+++ Shaun_Faulkner_ST10034664_PROG6212_POE/Services/BlobStorageService.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Storage.Blobs;
+
+namespace Shaun_Faulkner_ST10034664_PROG6212_POE.Services
+{
+    public class BlobStorageService
+    {
+        private readonly string _blobConnectionString;
+        private readonly string _blobContainerName;
+
+        public BlobStorageService(string blobconnectionString, string blobContainerName)
+        {
+            _blobConnectionString = blobconnectionString;
+            _blobContainerName = blobContainerName;
+        }
+
+        public async Task<string> UploadFileAsync(Stream fileStream, string fileName)
+        {
+            BlobContainerClient containerClient = new BlobContainerClient(_blobConnectionString, _blobContainerName);
+            await containerClient.CreateIfNotExistsAsync(Azure.Storage.Blobs.Models.PublicAccessType.Blob);
+
+            BlobClient blobClient = containerClient.GetBlobClient(fileName);
+            await blobClient.UploadAsync(fileStream, overwrite: true);
+
+            return blobClient.Uri.ToString();
+        }
+    }
+}
